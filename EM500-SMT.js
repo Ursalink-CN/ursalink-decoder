9841164d7756a6430426c231a542811917b616af
Terry Moore <tmm@mcci.com>
2020-09-26 13:25:37 -0400

    Update EMT500-SMT to decode system messages, a little more robustness

--- EM500-SMT.js
+++ EM500-SMT.js
@@ -1,3 +1,18 @@
+/*
+
+Name:   EM500-SMT.js
+
+Function:
+    Decode Ursalink soil-monitoring port 85 messages for TTN console.
+
+Copyright and License:
+    See accompanying LICENSE file at https://github.com/mcci-catena/MCCI-Catena-PMS7003/
+
+Author:
+    Terry Moore, MCCI Corporation   August 2020
+
+*/
+
 /**
  * Ursalink Sensor Payload Decoder
  *
@@ -12,7 +27,20 @@
 function Decoder(bytes, port) {
     var decoded = {};
 
+    // accept either no port at all, or port 85
+    if (! (port === undefined || port === 85)) {
+        return null;
+    }
+
+    decoded.Error = false;
     for (var i = 0; i < bytes.length;) {
+        // be robust
+        if (i > bytes.length - 2) {
+            // out of data.
+            decoded.Error = true;
+            break;
+        }
+
         var channel_id = bytes[i++];
         var channel_type = bytes[i++];
         // BATTERY
@@ -34,7 +62,47 @@ function Decoder(bytes, port) {
         else if (channel_id === 0x05 && channel_type === 0x7f) {
             decoded.conductivity= readInt16LE(bytes.slice(i, i + 2)) ;
             i += 2;
-        }  else {
+        }
+        // SYSTEM
+        else if (channel_id === 0xFF) {
+            // FORMAT_VERSION
+            if (channel_type === 0x01) {
+                decoded.FormatVersion = bytes[i++];
+            }
+            // HARDWARE_VERSION
+            else if (channel_type === 0x09) {
+                decoded.HardwareVersion = readVersion(bytes.slice(i, i+2));
+                i += 2;
+            }
+            // SOFTWARE_VERSION
+            else if (channel_type === 0x0A) {
+                decoded.SoftwareVersion = readVersion(bytes.slice(i, i+2));
+                i += 2;
+            }
+            // RESTART
+            if (channel_type === 0x0B) {
+                decoded.restart = 1;
+                ++i;
+            }
+            // POWER OFF
+            if (channel_type === 0x0C) {
+                decoded.shutdown = 1;
+                ++i;
+            }
+            // CLASS
+            else if (channel_type === 0x0F) {
+                decoded.Class = bytes[i++];
+            }
+            // SERIAL_NUMBER
+            else if (channel_type === 0x16) {
+                decoded.SerialNumber = readHexBytes(bytes.slice(i, i+6));
+                i += 6;
+            } else {
+                decoded.Error = true;
+                break;
+            }
+        } else {
+            decoded.error = true;
             break;
         }
     }
