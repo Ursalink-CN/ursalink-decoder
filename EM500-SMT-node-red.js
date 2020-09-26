b36640c42fb765163abe6940f564009aa6f67950
Terry Moore <tmm@mcci.com>
2020-09-26 13:29:24 -0400

    Ready EM500-SMT for testing

--- EM500-SMT-node-red.js
+++ EM500-SMT-node-red.js
@@ -1,3 +1,36 @@
+//
+// Module:  EMT500-SMT-node-red.js
+//
+// Function:
+//      This Node-RED decoding function decodes the record sent by the Ursalink
+//      EMT500-SMT sensor
+//
+// License:
+//      Copyright (C) 2020, MCCI Corporation.
+//      See LICENSE in accompanying git repository.
+//
+
+// calculate dewpoint (degrees C) given temperature (C) and relative humidity (0..100)
+// from http://andrew.rsmas.miami.edu/bmcnoldy/Humidity.html
+// rearranged for efficiency and to deal sanely with very low (< 1%) RH
+function dewpoint(t, rh) {
+	var c1 = 243.04;
+	var c2 = 17.625;
+	var h = rh / 100;
+	if (h <= 0.01)
+	    h = 0.01;
+	else if (h > 1.0)
+	    h = 1.0;
+    
+	var lnh = Math.log(h);
+	var tpc1 = t + c1;
+	var txc2 = t * c2;
+	var txc2_tpc1 = txc2 / tpc1;
+    
+	var tdew = c1 * (lnh + txc2_tpc1) / (c2 - lnh - txc2_tpc1);
+	return tdew;
+    }
+    
 /*
 
 Name:   EM500-SMT.js
@@ -120,3 +153,56 @@ function readInt16LE(bytes) {
     var ref = readUInt16LE(bytes);
     return ref > 0x7fff ? ref - 0x10000 : ref;
 }
+/*
+
+Node-RED function body.
+
+Input:
+    msg     the object to be decoded.
+
+            msg.payload_raw is taken
+            as the raw payload if present; otheriwse msg.payload
+            is taken to be a raw payload.
+
+            msg.port is taken to be the LoRaWAN port nubmer.
+
+
+Returns:
+    This function returns a message body. It's a mutation of the
+    input msg; msg.payload is changed to the decoded data, and
+    msg.local is set to additional application-specific information.
+
+*/
+
+var b;
+
+if ("payload_raw" in msg) {
+    // the console already decoded this
+    b = msg.payload_raw;  // pick up data for convenience
+    // msg.payload_fields still has the decoded data
+}
+else {
+    // no console debug
+    b = msg.payload;  // pick up data for conveneince
+}
+
+var result = Decoder(b, msg.port);
+
+// dewpoint might be interesting, so compute and store it.
+if ("temperature" in result && "humidity" in result)
+    {
+    result.tDewC = dewpoint(result.temperature, result.humidity);
+    }
+
+// now update msg with the new payload and new .local field
+// the old msg.payload is overwritten.
+msg.payload = result;
+msg.local =
+    {
+        nodeType: "Ursalink EM500-SMT",
+        platformType: "Urasalink EM500",
+        radioType: "Ursalink",
+        applicationName: "Soil Monitor"
+    };
+
+return msg;
