111921af99064848f4df3e8901cf13b4719fd94e
Terry Moore <tmm@mcci.com>
2020-09-26 13:01:38 -0400

    Add decoders

--- AM100-node-red.js
+++ AM100-node-red.js
@@ -1,3 +1,130 @@
+//
+// Module:  AM100-node-red.js
+//
+// Function:
+//      This Node-RED decoding function decodes the record sent by the Ursalink
+//      AM102 sensor
+//
+// License:
+//      Copyright (C) 2020, MCCI Corporation.
+//      See LICENSE in accompanying git repository.
+//
+
+// calculate dewpoint (degrees C) given temperature (C) and relative humidity (0..100)
+// from http://andrew.rsmas.miami.edu/bmcnoldy/Humidity.html
+// rearranged for efficiency and to deal sanely with very low (< 1%) RH
+function dewpoint(t, rh) {
+    var c1 = 243.04;
+    var c2 = 17.625;
+    var h = rh / 100;
+    if (h <= 0.01)
+        h = 0.01;
+    else if (h > 1.0)
+        h = 1.0;
+
+    var lnh = Math.log(h);
+    var tpc1 = t + c1;
+    var txc2 = t * c2;
+    var txc2_tpc1 = txc2 / tpc1;
+
+    var tdew = c1 * (lnh + txc2_tpc1) / (c2 - lnh - txc2_tpc1);
+    return tdew;
+}
+
+/*
+
+Name:   CalculateHeatIndex()
+
+Description:
+        Calculate the NWS heat index given dry-bulb T and RH
+
+Definition:
+        function CalculateHeatIndex(t, rh) -> value or null
+
+Description:
+        T is a Farentheit temperature in [76,120]; rh is a
+        relative humidity in [0,100]. The heat index is computed
+        and returned; or an error is returned.
+
+Returns:
+        number => heat index in Farenheit.
+        null => error.
+
+References:
+        https://github.com/mcci-catena/heat-index/
+        https://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
+
+        Results was checked against the full chart at iweathernet.com:
+        https://www.iweathernet.com/wxnetcms/wp-content/uploads/2015/07/heat-index-chart-relative-humidity-2.png
+
+        The MCCI-Catena heat-index site has a test js script to generate CSV to
+        match the chart, a spreadsheet that recreates the chart, and a
+        spreadsheet that compares results.
+
+*/
+
+function CalculateHeatIndex(t, rh) {
+    var tRounded = Math.floor(t + 0.5);
+
+    // return null outside the specified range of input parameters
+    if (tRounded < 76 || tRounded > 126)
+        return null;
+    if (rh < 0 || rh > 100)
+        return null;
+
+    // according to the NWS, we try this first, and use it if we can
+    var tHeatEasy = 0.5 * (t + 61.0 + ((t - 68.0) * 1.2) + (rh * 0.094));
+
+    // The NWS says we use tHeatEasy if (tHeatHeasy + t)/2 < 80.0
+    // This is the same computation:
+    if ((tHeatEasy + t) < 160.0)
+            return tHeatEasy;
+
+    // need to use the hard form, and possibly adjust.
+    var t2 = t * t;         // t squared
+    var rh2 = rh * rh;      // rh squared
+    var tResult =
+        -42.379 +
+        (2.04901523 * t) +
+        (10.14333127 * rh) +
+        (-0.22475541 * t * rh) +
+        (-0.00683783 * t2) +
+        (-0.05481717 * rh2) +
+        (0.00122874 * t2 * rh) +
+        (0.00085282 * t * rh2) +
+        (-0.00000199 * t2 * rh2);
+
+    // these adjustments come from the NWA page, and are needed to
+    // match the reference table.
+    var tAdjust;
+    if (rh < 13.0 && 80.0 <= t && t <= 112.0)
+        tAdjust = -((13.0 - rh) / 4.0) * Math.sqrt((17.0 - Math.abs(t - 95.0)) / 17.0);
+    else if (rh > 85.0 && 80.0 <= t && t <= 87.0)
+        tAdjust = ((rh - 85.0) / 10.0) * ((87.0 - t) / 5.0);
+    else
+        tAdjust = 0;
+
+    // apply the adjustment
+    tResult += tAdjust;
+
+    // finally, the reference tables have no data above 183 (rounded),
+    // so filter out answers that we have no way to vouch for.
+    if (tResult >= 183.5)
+        return null;
+    else
+        return tResult;
+}
+
+function CalculateHeatIndexCelsius(t, rh) {
+    var result = CalculateHeatIndex(t, rh);
+    if (result !== null) {
+        // convert to celsius.
+        result = (result - 32) * 5 / 9;
+    }
+    return result;
+}
+//--- end of MCCI heat index calculator ---
+// begin AM100.js
 /*
 
 Name:   AM100.js
@@ -186,3 +313,57 @@ function readHexBytes(bytes) {
     }
     return result.substr(1);
 }
+
+/*
+
+Node-RED function body.
+
+Input:
+    msg     the object to be decoded.
+
+            msg.payload_raw is taken
+            as the raw payload if present; otheriwse msg.payload
+            is taken to be a raw payload.
+
+            msg.port is taken to be the LoRaWAN port nubmer.
+
+
+Returns:
+    This function returns a message body. It's a mutation of the
+    input msg; msg.payload is changed to the decoded data, and
+    msg.local is set to additional application-specific information.
+
+*/
+
+var b;
+
+if ("payload_raw" in msg) {
+    // the console already decoded this
+    b = msg.payload_raw;  // pick up data for convenience
+    // msg.payload_fields still has the decoded data
+}
+else {
+    // no console debug
+    b = msg.payload;  // pick up data for conveneince
+}
+
+var result = Decoder(b, msg.port);
+
+if ("temperature" in result && "humidity" in result)
+    {
+    result.tDewC = dewpoint(result.temperature, result.humidity);
+    result.tHeatIndexC = CalculateHeatIndexCelsius(result.temperature, result.humidity);
+    }
+
+// now update msg with the new payload and new .local field
+// the old msg.payload is overwritten.
+msg.payload = result;
+msg.local =
+    {
+        nodeType: "Ursalink AM102",
+        platformType: "Urasalink AM100",
+        radioType: "Ursalink",
+        applicationName: "Ambience Monitor"
+    };
+
+return msg;
